53df6820d14f34cfae994c62cdd637402953371e
laurensgalarza <92804326+laurensgalarza@users.noreply.github.com>
2024-11-06 14:49:26 -0500

    Initial Commit

---
+++ WindDown/WindDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindDownApp.swift
+//  WindDown
+//
+//  Created by Lauren Galarza on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WindDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WindDownTests/WindDownTests.swift
@@ -0,0 +1,17 @@
+//
+//  WindDownTests.swift
+//  WindDownTests
+//
+//  Created by Lauren Galarza on 11/6/24.
+//
+
+import Testing
+@testable import WindDown
+
+struct WindDownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
